9b690a434dffa5503373629519347b9b39ee65d8
jony <jonyxdxp@gmail.com>
2026-05-24 11:40:04 -0300

    v6 s2

--- v6/s2/cog_arch/dm.py
+++ v6/s2/cog_arch/dm.py
@@ -1,4 +1,7 @@
 
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import torch
 import torch.nn as nn
 from config import cfg
--- v6/s2/data/data.py
+++ v6/s2/data/data.py
@@ -1,4 +1,7 @@
 
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 from tqdm.auto import tqdm
